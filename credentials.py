60e2af8e09093df410643969db5e45017481f372
RajpootPratima <singh.pratima143@gmail.com>
2017-06-21 18:24:35 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = "AC0697794b7c3c46c4e7afde63e2cab59a"
-auth_token = "5e6e73b35df0ccf77aca7ebfb3c4c592"
-my_cell = "+917022550092"
-my_twilio = "+12013550716"
+account_sid = ""
+auth_token = ""
+my_cell = ""
+my_twilio = ""
