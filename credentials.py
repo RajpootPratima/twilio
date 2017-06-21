848af42169d9773f848bad857a37e6c36be29ed5
RajpootPratima <singh.pratima143@gmail.com>
2017-06-21 18:15:11 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = "Your Account Sid"
-auth_token = "Your Auth Token"
-my_cell = "+Your Cell Number"
-my_twilio = "+Your Twilio Number"
+account_sid = "AC0697794b7c3c46c4e7afde63e2cab59a"
+auth_token = "5e6e73b35df0ccf77aca7ebfb3c4c592"
+my_cell = "+917022550092"
+my_twilio = "+12013550716"
